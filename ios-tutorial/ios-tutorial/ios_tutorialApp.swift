a3e22c3aa6978ca9efe462bd451e357dc29ca0b8
Student3 <student3@NIBMADMIN02s-iMac.local>
2026-06-06 16:35:14 +0530

    First Commit

---
+++ ios-tutorial/ios-tutorial/ios_tutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  ios_tutorialApp.swift
+//  ios-tutorial
+//
+//  Created by Student3 on 2026-06-06.
+//
+
+import SwiftUI
+
+@main
+struct ios_tutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
