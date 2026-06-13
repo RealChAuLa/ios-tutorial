62cac90e9eac3e0a1768afa0e7cbeb8c788d2c9f
Chalana Devinda <86785480+RealChAuLa@users.noreply.github.com>
2026-06-13 12:41:26 +0530

    Added Score State & UI changes (Spacer , Frame)

--- ios-tutorial/ios-tutorial/ContentView.swift
+++ ios-tutorial/ios-tutorial/ContentView.swift
@@ -8,15 +8,21 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var score = 0
+    
     var body: some View {
-        VStack {
+        VStack(spacing: 40) {
             Text("Tap Frenzy")
                 .font(.title)
             
-            Text("Score: 0")
+            Text("Score: \(score)")
                 .font(.system(size: 32))
             
-            Button(action: {}) {
+            Spacer()
+            
+            Button(action: {
+                score += 1
+            }) {
                 Text("TAP ME")
                     .font(.title)
                     .foregroundColor(.white)
@@ -24,7 +30,11 @@ struct ContentView: View {
                     .background(Color.blue)
                     .clipShape(Circle())
             }
+            
+            Spacer()
         }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(Color.gray.opacity(0.1))
     }
 }
 
