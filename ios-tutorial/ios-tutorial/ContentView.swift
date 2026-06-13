900469f3ca36ddd139e6bc1c5a749f13fee9ebd1
Chalana Devinda <86785480+RealChAuLa@users.noreply.github.com>
2026-06-13 11:22:12 +0530

    Update ContentView.swift
    removed the Default Content & Added the Title and the score text , and a round button

--- ios-tutorial/ios-tutorial/ContentView.swift
+++ ios-tutorial/ios-tutorial/ContentView.swift
@@ -10,12 +10,21 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Tap Frenzy")
+                .font(.title)
+            
+            Text("Score: 0")
+                .font(.system(size: 32))
+            
+            Button(action: {}) {
+                Text("TAP ME")
+                    .font(.title)
+                    .foregroundColor(.white)
+                    .frame(width: 150, height: 150)
+                    .background(Color.blue)
+                    .clipShape(Circle())
+            }
         }
-        .padding()
     }
 }
 
