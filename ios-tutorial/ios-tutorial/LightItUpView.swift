e2e1a829ddb73c034c427101d8775c010d3cb1c3
RealChAuLa <chalanadevinda1@gmail.com>
2026-06-15 11:38:03 +0530

    LightItUp - feat: light up a random square on a timer & when tapping
    Deleted ContentView.swift

--- ios-tutorial/ios-tutorial/LightItUpView.swift
+++ ios-tutorial/ios-tutorial/LightItUpView.swift
@@ -6,13 +6,27 @@
 //
 
 import SwiftUI
+import Combine
 
 struct LightItUpView: View {
+    @State private var litIndex = 0
+    @State private var timer = Timer.publish(every: 0.5, on: .main, in: .common).autoconnect()
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack(spacing: 20) {
+            ForEach(0..<3, id: \.self) { index in
+                RoundedRectangle(cornerRadius: 12)
+                    .fill(index == litIndex ? Color.green : Color.gray)
+                    .frame(width: 100, height: 100)
+                    .onTapGesture {litIndex = index
+                    }
+            }
+        }
+        .onReceive(timer) { _ in
+            litIndex = Int.random(in: 0..<3)
+        }
     }
 }
-
 #Preview {
     LightItUpView()
 }
