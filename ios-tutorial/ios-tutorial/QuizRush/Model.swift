d193ad9026efc4223230e829efe4599c6324d702
RealChAuLa <chalanadevinda1@gmail.com>
2026-07-03 14:08:18 +0530

    fix: Decode common HTML entities in model

--- ios-tutorial/ios-tutorial/QuizRush/Model.swift
+++ ios-tutorial/ios-tutorial/QuizRush/Model.swift
@@ -6,11 +6,12 @@
 //
 import Foundation
 
-struct TriviaResponse: Codable {
+// Changed from Codable to Decodable since we only read data, not send it
+struct TriviaResponse: Decodable {
     let results: [TriviaQuestion]
 }
 
-struct TriviaQuestion: Codable {
+struct TriviaQuestion: Decodable {
     let question: String
     let correctAnswer: String
     let incorrectAnswers: [String]
@@ -19,5 +20,31 @@ struct TriviaQuestion: Codable {
         case question
         case correctAnswer = "correct_answer"
         case incorrectAnswers = "incorrect_answers"
+    }
+    
+    // Custom decoder initializer to clean the strings the moment they arrive
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        
+        let rawQuestion = try container.decode(String.self, forKey: .question)
+        self.question = rawQuestion.replacingHTMLEntities()
+        
+        let rawCorrectAnswer = try container.decode(String.self, forKey: .correctAnswer)
+        self.correctAnswer = rawCorrectAnswer.replacingHTMLEntities()
+        
+        let rawIncorrectAnswers = try container.decode([String].self, forKey: .incorrectAnswers)
+        self.incorrectAnswers = rawIncorrectAnswers.map { $0.replacingHTMLEntities() }
+    }
+}
+
+// Your extension stays exactly the same
+extension String {
+    func replacingHTMLEntities() -> String {
+        return self
+            .replacingOccurrences(of: "&quot;", with: "\"")
+            .replacingOccurrences(of: "&#039;", with: "'")
+            .replacingOccurrences(of: "&amp;", with: "&")
+            .replacingOccurrences(of: "&lt;", with: "<")
+            .replacingOccurrences(of: "&gt;", with: ">")
     }
 }
